75273b9c2b9faaf292c2dbaa3ab9f410c668369f
markvarga21 <vmark2145@gmail.com>
2021-02-27 14:54:36 +0100

    drill13-refresh

--- GUI/drillek/drill13.cpp
+++ GUI/drillek/drill13.cpp
@@ -1,5 +1,5 @@
 /*
-    g++ main.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o main `fltk-config --ldflags --use-images`
+    g++ drill13.cpp ../source/Graph.cpp ../source/Window.cpp ../source/GUI.cpp ../source/Simple_window.cpp -o drill13 `fltk-config --ldflags --use-images`
 */
 #include "../source/Simple_window.h"
 #include "../source/Graph.h"
@@ -11,9 +11,9 @@ try {
     using namespace Graph_lib;
 
     // 1. feladat
-    const int width = 800;
-    const int heigth = 1000;
-    Simple_window win{Point{100,100}, width, heigth, "Drill 13 window"};   
+    const int width = 1000;
+    const int heigth = 800;
+    Simple_window win{Point{10,10}, width, heigth, "Drill 13 window"};   
     // 1. feladat vege
 
     // 2. feladat
@@ -23,60 +23,48 @@ try {
     const int grid_y = 100;
     Lines grid;
     //vertical
-    for (int i = 0; i <= width; i+=grid_x)
+    for (int i = 0; i <= heigth; i+=grid_x)
         grid.add(Point{i,0}, Point{i, heigth});
     //horizontal
     for (int i = 0; i <= heigth; i+=grid_x)
-        grid.add(Point{0,i}, Point{width, i});
+        grid.add(Point{0,i}, Point{heigth, i});
     // 2. feladat vege
 
     //a constot tudjuk valtoztatni pl const int image = valtoz(a);
     // a constexpr ismernunk kell forditasi idoben az erteket
 
     // 3. feladat
-    /*
-    Rectangle rect1{Point{0, 0}, grid_x, grid_y};
-    rect1.set_fill_color(Color::red);
-
-    Rectangle rect2{Point{grid_x, grid_y}, grid_x, grid_y};
-    rect2.set_fill_color(Color::red);
-
-    Rectangle rect3{Point{2*grid_x, 2*grid_y}, grid_x, grid_y};
-    rect3.set_fill_color(Color::red);
-
-    Rectangle rect4{Point{3*grid_x, 3*grid_y}, grid_x, grid_y};
-    rect4.set_fill_color(Color::red);
-
-    Rectangle rect5{Point{4*grid_x, 4*grid_y}, grid_x, grid_y};
-    rect5.set_fill_color(Color::red);
-
-    Rectangle rect6{Point{5*grid_x, 5*grid_y}, grid_x, grid_y};
-    rect6.set_fill_color(Color::red);
-
-    Rectangle rect7{Point{6*grid_x, 6*grid_y}, grid_x, grid_y};
-    rect7.set_fill_color(Color::red);
-
-    Rectangle rect8{Point{7*grid_x, 7*grid_y}, grid_x, grid_y};
-    rect8.set_fill_color(Color::red);
-    */
-    // 3. feladat vege
-    /*
-    vector<Rectangle> rect(8);
-    for (int i = 0; i <= 8; i++){
-        rect[i]{Point{i*grid_x, i*grid_y}, grid_x, grid_y};
+    Vector_ref<Rectangle> rect;
+    for (int i = 0; i < 8; ++i){
+        rect.push_back(new Rectangle({i*grid_x, i*grid_y}, grid_x, grid_y));
         rect[i].set_fill_color(Color::red);
+        rect[i].set_color(Color::invisible);
         win.attach(rect[i]);
-    }*/
-
+    }
+    // 3. feladat vege
+    
     // 4. feladat
     Image i1{Point{0, 0}, "kep200.jpg"};
     Image i2{Point{200, 200}, "kep200.jpg"};
     Image i3{Point{400,400}, "kep200.jpg"};
+    // 4. feladat vege
 
-    win.attach(i1); win.attach(i2); win.attach(i3); //4
     win.attach(grid); //2
-    //win.attach(rect1); win.attach(rect2); win.attach(rect3); win.attach(rect4); win.attach(rect5); win.attach(rect6); win.attach(rect7); win.attach(rect8);//3
-    win.wait_for_button();
+    win.attach(i1); win.attach(i2); win.attach(i3); //4
+
+    //5. feladat
+    Image kicsikep{Point{0, 0}, "100as.jpg"};
+    win.attach(kicsikep);
+
+    int cnt = 1;
+    while (cnt <= 64){
+        win.wait_for_button();
+        if (cnt % 8 == 0) kicsikep.move(-800, 100);   
+        kicsikep.move(100, 0);
+        cnt++;
+    }
+    //5. feladat vege
+
     return 0;
 } catch (exception& e){
     cerr << e.what() << endl;
